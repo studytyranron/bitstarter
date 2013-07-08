defada18007354bee348e8c1cb11db4f41242791
studytyranron <study.tyranron@gmail.com>
2013-07-08 23:41:53 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+var express = require('express'),
+    fs = require('fs'),
+    app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
